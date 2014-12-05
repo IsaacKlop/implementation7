dc8ae3b9bba570896c7e5589f7725be114dc11ae
Isaac Klop <Isaac.Klop@hva.nl>
2014-12-05 16:42:18 +0100

    Toevoegen Eclipse project

---
+++ GcmServer/src/gcmserver/App.java
@@ -0,0 +1,35 @@
+package gcmserver;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gcmserver.Content;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Sending POST to GCM" );
+
+        String apiKey = "AIzaSyC3clKuTxILxby8euNiyO9dqTJy2wqCWcg";
+        Content content = createContent();
+
+        PostGcm.post(apiKey, content);
+    }
+
+    public static Content createContent(){
+
+        Content c = new Content();
+
+        c.addRegId("APA91bEXu_2F1q0tBggVJwehyvdamnHO7yqzHV85JZ_jtOYGPqCGg2WLmy0bk6SJJBweyU1pIwHcgiDsOD6tDnUMxcSbsCuX8T_hjaOLUz15Q8yH1W94OcKaNmCzOzSpR3aAfyrxHsgcGJl7VWqWhA3oBFPabyBvphIvrsadh9LIzXsye9fuJts");
+        c.createData("Hoi Alex", "Test Message");
+
+        return c;
+    }
+}

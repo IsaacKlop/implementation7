2873860a13047ebcda4b55496c893aa4a60dbeb7
Jast Hamelink <jast.hamelink@hva.nl>
2014-12-10 16:18:47 +0100

    servet update

--- GcmServer2/src/servlet1/servlet1.java
+++ GcmServer2/src/servlet1/servlet1.java
@@ -11,13 +11,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 /**
  * Servlet implementation class uuid
  */

45fe89bb3c7ae8f92bdc49bcbe5386e76d88e452
Jast Hamelink <jast.hamelink@hva.nl>
2014-12-10 15:46:52 +0100

    servlet1

---
+++ GcmServer2/src/servlet1/servlet1.java
@@ -0,0 +1,59 @@
+package servlet1;
+
+import database.UserDao;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Servlet implementation class uuid
+ */
+@WebServlet("/servlet1")
+public class servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public servlet1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		//Generate UUID
+		Long uuidGet = UUID.randomUUID().getMostSignificantBits();
+		
+		String uuid = Long.toString(uuidGet);
+		
+		String username = "bla";
+		
+		UserDao userDao = new UserDao();
+		userDao.setUuid(uuid, username);
+	}
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
